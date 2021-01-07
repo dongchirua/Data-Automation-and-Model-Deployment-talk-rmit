109bbff88a007c77d60fabaa64822d73d27a6b71
Quy Nguyen <v.quyng@vinbrain.net>
2021-01-08 05:02:43 +0700

    step 2

---
+++ main_2.py
@@ -0,0 +1,76 @@
+from flask import Flask, request, jsonify
+import joblib
+import pandas as pd
+import json
+from sklearn.base import TransformerMixin, BaseEstimator
+
+def AvgMinutes(Count, Duration):
+    if Duration == 0:
+        output = 0
+    elif Duration != 0:
+        output = float(Duration)/float(Count)
+    return output
+    
+Columns = ['Administrative', 'Administrative_Duration',
+           'Informational', 'Informational_Duration',
+           'ProductRelated', 'ProductRelated_Duration']
+
+class CustomTransform(BaseEstimator, TransformerMixin):
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X, y=None):
+        X['AvgAdministrative'] = X.apply(lambda x: AvgMinutes(Count = x['Administrative'], Duration = x['Administrative_Duration']), axis = 1)
+        X['AvgInformational'] = X.apply(lambda x: AvgMinutes(Count = x['Informational'], Duration = x['Informational_Duration']), axis = 1)
+        X['AvgProductRelated'] = X.apply(lambda x: AvgMinutes(Count = x['ProductRelated'], Duration = x['ProductRelated_Duration']), axis = 1)
+        X.drop(['Administrative', 'Administrative_Duration','Informational', 'Informational_Duration','ProductRelated', 'ProductRelated_Duration'], axis = 1, inplace = True)
+        return X[['AvgAdministrative', 'AvgInformational', 'AvgProductRelated']]
+
+app = Flask(__name__)
+
+# inputs
+model_file_name = './BDSFinalProject-master/model.pkl'
+
+# These will be populated at training time
+clf = None
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    if clf:
+        try:
+            json_ = request.json
+
+            # import pdb; pdb.set_trace()
+
+            query = pd.read_json(json.dumps(json_), orient='records')
+
+            prediction = list(clf.predict(query))
+
+            # Converting to int from int64
+            return jsonify({"prediction": list(map(int, prediction))})
+
+        except Exception as e:
+
+            return jsonify({'error': str(e), 'trace': traceback.format_exc()})
+    else:
+        print('train first')
+        return 'no model here'
+
+
+if __name__ == '__main__':
+    try:
+        port = int(sys.argv[1])
+    except Exception as e:
+        port = 8889
+
+    try:
+        clf = joblib.load(model_file_name)
+        print('model loaded')
+    except Exception as e:
+        print('No model here')
+        print('Train first')
+        print(str(e))
+        clf = None
+
+    app.run(host='0.0.0.0', port=port, debug=True)
